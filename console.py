39701fd773be6bd2d277950c36ae7879e67c289b
Freeboard Tortoise <freeboardtortoise@proton.me>
2025-03-27 16:03:28 +0000

    added the usernames plugin and updated the console plugin to use the usernames plugin if it is in the plugins.txt file

--- console.py
+++ console.py
@@ -1,14 +1,21 @@
 import TSC.server
 import TSC.client
-import time
-import random
 
 connections = []
+importedThings = []
 def init():
-    global connections
+    global connections,  importedThings
     print("this project uses the TSC-console extension made by Philip G Hall")
+    file = open("TSC/plugins/plugins.txt","r")
+    for line in (file.read()).split("\n"):
+        if line == "console":
+            pass
+        if line == "usernames":
+            import usernames
+            importedThings = importedThings + ["usernames"]
 
 def console_thing(connections):
+    global importedThings
     command = input(">>>")
 
     commandSections = len(command.split(" "))
@@ -16,25 +23,41 @@ def console_thing(connections):
         if command == "help":
             print("command list")
             print("help                 lists all commands")
-            print("kick <ip>            removes a person from the server")
-            print("send <ip> <what>     sends a message to a person on the server")
+            if "usernames" in importedThings:
+                print("kick <username>    remove a person from the server")
+                print("send <username>    sends the person sellected a message")
+            else:
+                print("kick <ip>            removes a person from the server")
+                print("send <ip> <what>     sends a message to a person on the server")
             print("quit                 quits the program")
-            print("custom               can be programmed to do anything you like it to do")
         if command == "list":
             for connection in connections:
-                print(f"ipv4: {str(connection[1])}, ID: {str(connection[2])}")
+                if "usernames" in importedThings:
+                    print(f"ipv4: {str(connection[1])}, ID: {str(connection[2])}, Username: {usernames.get_username(connection[1])}")
+                else:
+                    print(f"ipv4: {str(connection[1])}, ID: {str(connection[2])}")
     elif commandSections == 2:
         if command.split(" ")[0] == "kick":
             for connection in connections:
-                if (connection[1])[0] == command.split(" ")[1]: #conn, addr, ID
-                    conn = connection[0]
-                    conn.close()
+                if "usernames" in importedThings:
+                    if connection[1][0] == usernames.get_username(command.split(" ")[1]):
+                        conn = connection[0]
+                        conn.close()
+                else:
+                    if (connection[1])[0] == command.split(" ")[1]: #conn, addr, ID
+                        conn = connection[0]
+                        conn.close()
     elif commandSections == 3:
         if command.split(" ")[0] == "send":
             for connection in connections:
-                if connection[1][0] == command.split(" ")[1]:
-                    conn = connection[0]
-                    TSC.server.send(conn, command[2])
+                if "usernames" not in importedThings:
+                    if connection[1][0] == command.split(" ")[1]:
+                        conn = connection[0]
+                        TSC.server.send(conn, command[2])
+                else:
+                    if connection[1][0] == usernames.get_username(command.split(" ")[1]):
+                        conn = connection[0]
+                        TSC.server.send(conn, command[2])
     
     elif command == "quit":
         quit()
