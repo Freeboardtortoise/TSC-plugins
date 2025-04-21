1d9c9fdc9e559a6d82119278109365d2f6529712
Freeboard Tortoise <freeboardtortoise@proton.me>
2025-04-21 20:10:25 +0200

    added datatypes and fixed documentation

--- databases.py
+++ databases.py
@@ -1,3 +1,32 @@
+def _intify(string):
+    numbers = ['1','2','3','4','5','6','7','8','9','0']
+    for letter in string:
+        if letter in numbers:
+            pass
+        else:
+            return None
+
+    return int(string)
+
+def _is_int(number):
+    numbers = ['1','2','3','4','5','6','7','8','9','0']
+    for letter in str(number):
+        if letter in numbers:
+            pass
+        else:
+            return False
+    return True
+
+def _floatify(string):
+    numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.']
+    for letter in string:
+        if letter in numbers:
+            pass
+        else:
+            return None
+
+    return float(string)
+
 class Connection:
     def __init__(self, filename):
         self.file = filename
@@ -5,6 +34,7 @@ class Connection:
         self.splitter = "$|$"
         self.values = []
         self.tables = []
+        open(filename, 'a').close()
         file = open(filename, 'r')
         file_len = file.read()
         file.close()
@@ -21,7 +51,23 @@ class Connection:
                     if line[1] == "NEW":
                         self.tables.append(line[2])
                     if line[2] == "VALUE":
-                        self.values.append([line[1], line[3], line[4]])
+                        if line[3] == "STRING":
+                            self.values.append([line[1], line[3], line[4]])
+                        elif line[3] == "INT":
+                            value4 = _intify(line[4])
+                            if value4 == None:
+                                print("ERROR")
+                                print("INCORRECT DATATYPE")
+                            else:
+                                self.values.append([line[1], line[3], value4])
+                        elif line[3] == "FLOAT":
+                            value4 = _floatify(line[4])
+                            if value4 == None:
+                                print("ERROR")
+                                print("INCORRECT DATATYPE")
+                            else:
+                                self.values.append([line[1], line[3], value4])
+
                 if line[0] == "TSC-database=validated":
                     self.valid = True
 
@@ -38,13 +84,30 @@ class Connection:
     def clear(self):
         open(self.file, "w").close()
 
-    def add_value(self, table, key, value):
-        for theValue in self.values:
-            if theValue[0] == table:
-                if theValue[1] == key:
-                    return False
-        self.values = self.values + [[table, key, value]]
-        return False
+    def add_value(self, table, key, value, datatype='string'):
+        if datatype == 'string':
+            for theValue in self.values:
+                if theValue[0] == table:
+                    if theValue[1] == key:
+                        return False
+            self.values = self.values + [[table, key, value, "STRING"]]
+            return True
+        elif datatype == 'int':
+            for theValue in self.values:
+                if theValue[0] == table:
+                    if theValue[1] == key:
+                        return False
+            self.values = self.values + [[table, key, value, "INT"]]
+            return True
+        elif datatype == 'float':
+            for theValue in self.values:
+                if theValue[0] == table:
+                    if theValue[1] == key:
+                        return False
+            self.values = self.values + [[table, key, value, "FLOAT"]]
+            return True
+        else:
+            return False
 
     def verify(self):
         with open(self.file, 'r') as file:
@@ -57,16 +120,29 @@ class Connection:
                     self.valid = False
                     return False
         self.valid = True
+        for value in self.values:
+            if value[3] == "INT":
+                if _is_int(value[2]) == True:
+                    self.valid = False
+                    return False
+                if _is_int(value[2]) == True:
+                    self.valid = False
+                    return False
         return True
 
     def write(self):
-        with open(self.file, 'w') as file:
-            if self.valid == True:
-                file.write("TSC-database=validated\n")
-            for table in self.tables:
-                file.write(f"TABLE{self.splitter}NEW{self.splitter}{table}\n")
-            for value in self.values:
-                file.write(f"TABLE{self.splitter}{value[0]}{self.splitter}VALUE{self.splitter}{value[1]}{self.splitter}{value[2]}\n")
+        if self.valid:
+            with open(self.file, 'w') as file:
+                if self.valid == True:
+                    file.write("TSC-database=validated\n")
+                for table in self.tables:
+                    file.write(f"TABLE{self.splitter}NEW{self.splitter}{table}\n")
+                for value in self.values:
+                    file.write(f"TABLE{self.splitter}{value[0]}{self.splitter}VALUE{self.splitter}{value[1]}{self.splitter}{value[2]}{self.splitter}{value[3]}\n")
+        else:
+            print("ERROR")
+            print("INVALID DATABASE")
+            print("VERIFY DATABASE WITH conn.verify()")
 
     def read(self, sellectedValue='', table=''):
         returnedValues = []
@@ -107,3 +183,6 @@ class Connection:
             print("NO SUTCH TABLE")
             return 1
         print("NOTHING IN DATABASE")
+
+    def get_table_list(self):
+        return self.tables
