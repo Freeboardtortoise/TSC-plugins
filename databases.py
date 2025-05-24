4aa72585e85e7f32f3ae25d31670663b12190901
freeboardtortoise <darionkfitt@gmail.com>
2025-05-24 15:49:40 +0000

    changed the documentation added some better database functionality added database removal of tables and values

--- databases.py
+++ databases.py
@@ -17,6 +17,16 @@ def _is_int(number):
             return False
     return True
 
+def _is_float(string):
+    numbers = ['1','2','3','4','5','6','7','8','9','0','.']
+    for letter in str(string):
+        if letter in numbers:
+            pass
+        else:
+            return False
+
+    return True
+
 def _floatify(string):
     numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.']
     for letter in string:
@@ -35,13 +45,27 @@ def _check_file(filename):
         return False
 
 class Connection:
-    def __init__(self, filename, _encrypt=False):
+    def __init__(self, filename, encrypt=False, passphrase=None, passfile=None):
+        if encrypt:
+            if _check_file("TSC/plugins/encrypter.py") == True:
+                self.encrypt = True
+                if passfile != None:
+                    with open(passfile,"r") as file:
+                        self.passphrase = file.read()
+                elif passphrase != None:
+                    self.passphrase = passphrase
+            else:
+                print("ERROR")
+                print("encrypter package not installed or not in the correct directory")
+                print("install all pugins from the following commands")
+                print("git clone https://github.com/Freeboardtortoise/TSC-plugins.git")
+                print("then move all of them into the plugins directory in TSC/plugins")
         self.file = filename
         self.valid = False
         self.splitter = "$|$"
         self.values = []
         self.tables = []
-        self.encrypt = _encrypt
+        self.encrypt = encrypt
         if self.encrypt:
             pass
         else:
@@ -109,14 +133,11 @@ class Connection:
                                 value = _floatify(value)
                                 self.values.append([table, key, value])
 
-                if line[0] == "TSC-database=validated":
-                    self.valid = True
-
-    def make_table(self, name):
+    def add_table(self, name):
         if name in self.tables:
             print("ERROR")
             print(f"TABLE: {name} already exists")
-            return 1
+            quit()
         else:
             self.tables = self.tables + [name]
             return 0
@@ -129,21 +150,21 @@ class Connection:
             return 1
 
     def add_value(self, table, key, value, datatype='string'):
-        if datatype == 'string':
+        if datatype.lower() == 'string':
             for theValue in self.values:
                 if theValue[0] == table:
                     if theValue[1] == key:
                         return False
             self.values = self.values + [[table, key, value, "STRING"]]
             return True
-        elif datatype == 'int':
+        elif datatype.lower() == 'int':
             for theValue in self.values:
                 if theValue[0] == table:
                     if theValue[1] == key:
                         return False
             self.values = self.values + [[table, key, value, "INT"]]
             return True
-        elif datatype == 'float':
+        elif datatype.lower() == 'float':
             for theValue in self.values:
                 if theValue[0] == table:
                     if theValue[1] == key:
@@ -157,21 +178,18 @@ class Connection:
         with open(self.file, 'r') as file:
             file = file.read()
             file = file.split("\n")
-            file = file.pop(-1)
-            for line in file:
-                line = line.split(self.splitter)
-                if line[0] != "TABLE" or "TSC-database=validated":
-                    self.valid = False
-                    return False
+            file = file[1:]
+            for number, line in enumerate(file):
+                file[number] = line.split(self.splitter)
         self.valid = True
         for value in self.values:
             if value[3] == "INT":
                 if _is_int(value[2]) == True:
-                    self.valid = False
-                    return False
-                if _is_int(value[2]) == True:
-                    self.valid = False
-                    return False
+                    self.valid = True
+            elif value[3] == "FLOAT":
+                if _is_float(value[2]) == True:
+                    self.valid = True
+
         return True
 
     def write(self):
@@ -189,7 +207,12 @@ class Connection:
                     if self.encrypt == True:
                         table = e.encrypt(string=value[0], passphrase=self.passphrase)
                         key = e.encrypt(string=value[1], passphrase=self.passphrase)
-                        _value = e.encrypt(string=value[2], passphrase=self.passphrase)
+                        if value[3] == "STRING":
+                            _value = e.encrypt(string=value[2], passphrase=self.passphrase)
+                        elif value[3] == "INT":
+                            _value = e.encrypt(string=str(value[2]), passphrase=self.passphrase)
+                        elif value[3] == "FLOAT":
+                            _value = e.encrypt(string=str(value[2]), passphrase=self.passphrase)
                         file.write(f"TABLE{self.splitter}{table}{self.splitter}VALUE{self.splitter}{key}{self.splitter}{_value}{self.splitter}{value[3]}\n")
                     else:
                         file.write(f"TABLE{self.splitter}{value[0]}{self.splitter}VALUE{self.splitter}{value[1]}{self.splitter}{value[2]}{self.splitter}{value[3]}\n")
@@ -197,34 +220,31 @@ class Connection:
             print("ERROR")
             print("INVALID DATABASE")
             print("VERIFY DATABASE WITH conn.verify()")
+            quit()
 
-    def read(self, sellectedValue=None, table=None):
+    def read(self, key=None, table=None):
         returnedValues = []
-        if table == None and sellectedValue == None:
-            return self.values
-        elif sellectedValue == None and table != None:
+        if table == None and key == None:
+            for value in self.values:
+                returnedValues.append(value[:3])
+        elif key == None and table != None:
             for value in self.values:
                 if value[0] == table:
                     returnedValues = returnedValues + [[value[1],value[2]]]
-        elif sellectedValue != None and table == None:
+        elif key != None and table == None:
             for value in self.values:
-                if value[1] == sellectedValue:
+                if value[1] == key:
                     returnedValues.append(value[2])
-        elif sellectedValue != None and table != None:
+        elif key != None and table != None:
             for value in self.values:
                 if value[0] == table:
-                    if value[1] == sellectedValue:
+                    if value[1] == key:
                         return value[2]
                     else:
                         pass
         return returnedValues
 
-    def reset(self, _encrypt=False):
-        self.values = []
-        self.tables = []
-        self.__init__(self.file, _encrypt=_encrypt)
-
-    def edit(self, table, key, value):
+    def edit_value(self, table, key, value):
         for number, sellectedValue in enumerate(self.values):
             if sellectedValue[0] == table:
                 if sellectedValue[1] == key:
@@ -235,19 +255,46 @@ class Connection:
                 return 1
             print("ERROR")
             print("NO SUTCH TABLE")
-            return 1
+            quit()
         print("NOTHING IN DATABASE")
 
     def get_table_list(self):
         return self.tables
     
-    def encrypt_database(self, passphrase):
-        if _check_file("TSC/plugins/encrypter.py") == True:
-            self.encrypt = True
-            self.passphrase = passphrase
-            self.reset(_encrypt=True)
-            return 0
-        else:
-            print("ERROR")
-            print("encrypter.py not in directory ('TSC/plugins/encrypter.py')")
-            return 1
+    def remove_table(self, table):
+        # setting the new_values and new_tables variables
+        new_tables = []
+        new_values = []
+        # populating the new_values and new_tables varibles
+        for theTable in self.tables:
+            if theTable == table:
+                pass
+            else:
+                new_tables.append(theTable)
+        for value in self.values:
+            if value[0] == table:
+                pass
+            else:
+                new_values.append(value)
+        # setting the self.values and self.tables to the new_tables and new_values
+        self.tables = new_tables
+        self.values = new_values
+        return 0
+    
+    def remove_value(self, table, key):
+        new_values = []
+
+        for value in self.values:
+            if value[0] == table and value[1] == key:
+                pass
+            else:
+                new_values.append(value)
+        
+        self.values = new_values
+        return 0
+    
+    def close(self):
+        self.values = []
+        self.tables = []
+        self.passphrase = ""
+        return 0
--- testing.py
+++ testing.py
@@ -1,10 +1,17 @@
-import encrypter as e
-passfile = "passfile.txt"
+import databases
+conn = databases.Connection("database.tdb")
 
-if e._generate_passphrase(passfile=passfile) == True:
-    encrypted = e.encrypt(passfile=passfile, string="Tortoises are fast and clever")
-    decrypted = e.decrypt(passfile=passfile, string=encrypted)
-    print(encrypted)
-    print(decrypted)
-else:
-    print("falied")
+conn.add_table("tortoise table 1")
+conn.remove_table("tortoise table 1")
+conn.add_table("tortoise table")
+conn.add_value("tortoise table", "name", "Darion")
+conn.add_value("tortoise table", "age", 100, "int")
+conn.verify()
+conn.write()
+conn.close()
+
+conn = databases.Connection("database.tdb")
+conn.remove_value(table="tortoise table", key="name")
+conn.verify()
+conn.write()
+print(conn.read())
